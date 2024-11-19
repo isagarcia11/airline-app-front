f8959b3ee201a4d1d582e0b819bb716a10c1fc5b
isagarcia11 <isabella.garciag@uqvirtual.edu.co>
2024-11-19 09:01:36 -0500

    Método Login Actualizado

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../servicios/auth.service';
 import { Router } from '@angular/router';
+import { LoginDTO } from '../../dtos/login-dto';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,10 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm !: FormGroup;
+  correo: string = '';
+  contrasena: string = '';
+  mensaje: string = '';
+
   errorMessage: string | null = null;
   successMessage: string | null = null;
 
@@ -23,20 +28,22 @@ export class LoginComponent implements OnInit {
     contrasena: ['', [Validators.required, Validators.minLength(6)]],
   });
   }
+
   public login() {
-    if (this.loginForm.valid) {
-      const { correo, contrasena } = this.loginForm.value;
-      this.authService.login(correo, contrasena).subscribe({
-        next: (response) => {
-          this.router.navigate(['/registro']);
-          this.successMessage = response;
-          this.errorMessage = null;
-        },
-        error: (error) => {
-          this.errorMessage = error.error || 'Error inesperado.';
-          this.successMessage = null;
-        },
-      });
-    }
+    const loginDTO: LoginDTO = {
+      correo: this.correo,
+      contrasena: this.contrasena,
+    };
+
+    this.authService.login(loginDTO).subscribe(
+      (response) => {
+        this.mensaje = `Inicio de sesión exitoso: ${response}`;
+        console.log(response); // Maneja la respuesta según tus necesidades
+      },
+      (error) => {
+        this.mensaje = `Error al iniciar sesión: ${error.error}`;
+        console.error(error); // Maneja el error
+      }
+    );
   }
 }
