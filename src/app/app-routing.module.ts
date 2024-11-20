ec99df3435a7eb6f45729828edd3a914d7b85952
AnaVelez11 <anam.velezr@uqvirtual.edu.co>
2024-11-20 06:29:11 -0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,21 @@ import { FormGroup } from '@angular/forms';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { ContrasenaComponent } from './componentes/contrasena/contrasena.component';
+import { UsuarioMenuComponent } from './componentes/usuario-menu/usuario-menu.component';
+import { ReservaVueloComponent } from './componentes/reserva-vuelo/reserva-vuelo.component';
+import { BuscarVuelosComponent } from './componentes/buscar-vuelos/buscar-vuelos.component';
+import { GestionarEquipajeComponent } from './componentes/gestionar-equipaje/gestionar-equipaje.component';
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   {path:'registro', component: RegistroComponent},
-  {path:'contrasena',component:ContrasenaComponent}
+  {path:'contrasena',component:ContrasenaComponent},
+  { path: 'usuario-menu', component: UsuarioMenuComponent },
+  { path: 'reserva-vuelo', component: ReservaVueloComponent },
+  { path: 'buscar-vuelos', component: BuscarVuelosComponent },
+  { path: 'gestionar-equipaje', component: GestionarEquipajeComponent },
+
 
 ];
 
--- src/app/componentes/contrasena/contrasena.component.ts
+++ src/app/componentes/contrasena/contrasena.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'; // Para hacer las solicitudes HTTP
 
 @Component({
   selector: 'app-contrasena',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrl: './contrasena.component.css'
 })
 export class ContrasenaComponent {
+  contrasenaForm: FormGroup;
+  mensaje: string | null = null;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { 
+    this.contrasenaForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]]
+    });
+  };
+
+  ngOnInit() {
+   
+  }
+
+  enviarCorreo() {
+    if (this.contrasenaForm.valid) {
+      const correo = this.contrasenaForm.get('correo')?.value;
+      this.mensaje = 'Enviando código...';
+  
+      this.http.post('http://localhost:8080/api/auth/enviar-codigo-recuperacion', { correo })
+        .subscribe(
+          (response: any) => {
+            if (response.message) {
+              this.mensaje = response.message;  // Asegúrate de mostrar el mensaje correcto
+            } else {
+              this.mensaje = 'Hubo un problema al enviar el código.';
+            }
+          },
+          error => {
+            this.mensaje = 'Error en el servidor, intenta más tarde.';
+          }
+        );
+    } else {
+      this.mensaje = 'Por favor, ingresa un correo electrónico válido.';
+    }
+  }
+  
+  
+  
+
 
 }
